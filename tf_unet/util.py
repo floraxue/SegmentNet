b85f55873335063bce829a51f5f7f5575379b9f3
Flora Xue <miss.floraxue@gmail.com>
2019-05-10 23:35:05 +0000

    finish project

--- tf_unet/util.py
+++ tf_unet/util.py
@@ -23,6 +23,7 @@ import os
 
 import numpy as np
 from PIL import Image
+import matplotlib.pyplot as plt
 
 def plot_prediction(x_test, y_test, prediction, save=False):
     import matplotlib
@@ -141,3 +142,28 @@ def create_training_path(output_path, prefix="run_"):
         idx += 1
         path = os.path.join(output_path, "{:}{:03d}".format(prefix, idx))
     return path
+
+
+
+def show_images(images, cols = 1, titles = None):
+    """Display a list of images in a single figure with matplotlib.
+    
+    Parameters
+    ---------
+    images: List of np.arrays compatible with plt.imshow.
+    
+    cols (Default = 1): Number of columns in figure (number of rows is 
+                        set to np.ceil(n_images/float(cols))).
+    """
+    assert((titles is None)or (len(images) == len(titles)))
+    n_images = len(images)
+    if titles is None: titles = ['Image (%d)' % i for i in range(1,n_images + 1)]
+    fig = plt.figure()
+    for n, (image, title) in enumerate(zip(images, titles)):
+        a = fig.add_subplot(cols, np.ceil(n_images/float(cols)), n + 1)
+        if image.ndim == 2:
+            plt.gray()
+        plt.imshow(image)
+        a.set_title(title)
+    fig.set_size_inches(np.array(fig.get_size_inches()) * n_images)
+    plt.show()
